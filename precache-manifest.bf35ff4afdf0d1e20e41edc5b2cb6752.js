29dfde09ec6e34d99a25c06967d831b9942094bc
AmolPawar7181 <amol.pawar@hotmail.com>
2020-01-31 17:09:11 +0530

    files

---
+++ precache-manifest.bf35ff4afdf0d1e20e41edc5b2cb6752.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d346a4c1b4ceeb8cf1017f4bfb37299",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "65ea7e33c35f345fb540",
+    "url": "/react/static/css/main.21b83a88.chunk.css"
+  },
+  {
+    "revision": "aa981f2bcbed014bb2f5",
+    "url": "/react/static/js/2.c914e7df.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react/static/js/2.c914e7df.chunk.js.LICENSE"
+  },
+  {
+    "revision": "65ea7e33c35f345fb540",
+    "url": "/react/static/js/main.207ffe32.chunk.js"
+  },
+  {
+    "revision": "5fb850e80e985c492e88",
+    "url": "/react/static/js/runtime-main.c320cde7.js"
+  }
+]);
